4b0576910135d4960afe0c1cd8199863ed4976f1
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-22 22:12:14 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 function printRandomNumber() {
-    var num = Math.floor(Math.random() * 10);
-    return num;
+  var num = Math.floor(Math.random() * 10);
+  return num;
+}
+printRandomNumber();
+
+function randomNumber(num) {
+  var rnd = Math.round(Math.random() * num);
+    return rnd;
 }
 
-var sum = printRandomNumber();
+
+// function randonNumberOne(num) {
+//     var rnd = Math.round(Math.random() * 10);
+//     return rnd;
+    
+// }
+// var a=randonNumberOne(prompt());
+// console.log(a);
+
+
+function popRandomArray() {
+    var rndArray = [];
+    for (var i = 0; i <10; i++){
+        rndArray.push(Math.floor(Math.random()*10));
+
+    }
+    console.log(rndArray);
+}
+popRandomArray();
+
+
+function getRandomNumFromUser() {
+
+  var userNum = +prompt();
+  var rnd = Math.floor(Math.random() * 10)
+  if (userNum == rnd) {
+    alert("Good Job!");
+  }
+  else {
+    alert("Wrong");
+  }
+
+}
+
+var sum = getRandomNumFromUser();
 console.log(sum);
-
-
